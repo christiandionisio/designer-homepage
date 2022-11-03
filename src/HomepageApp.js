b231832bc4d0d5fdf4e4af4556fe96cb746a0dcf
Christian Dionisio <christiandionisio9432@gmail.com>
2022-11-03 18:28:03 -0500

    first commit

---
+++ src/HomepageApp.js
@@ -0,0 +1,8 @@
+
+function HomepageApp() {
+  return (
+    <h1>Homepage App</h1>
+  );
+}
+
+export default HomepageApp;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomepageApp from './HomepageApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HomepageApp />
   </React.StrictMode>
 );
 
