0cba6a4f9638b2ec691ab3ca6277dfb183be635a
Christian Dionisio <christiandionisio9432@gmail.com>
2022-11-03 19:31:14 -0500

    navbar styles

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,8 +1,18 @@
 
 export const Navbar = () => {
   return (
-    <nav>
-        Navbar
+    <nav className="navbar-section">
+        <span className="navbar-brand">
+            Homepage
+        </span>
+
+        <ul className="navbar-menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Our Works</a></li>
+            <li><a href="#">Clients</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
     </nav>
   )
 }
