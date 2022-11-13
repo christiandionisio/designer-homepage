750fc01ed0faf3a2d5515be07b67a1e63c67a718
Christian Dionisio <christiandionisio9432@gmail.com>
2022-11-12 19:08:00 -0500

    add menu icon

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 
 export const Navbar = () => {
+
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   return (
     <nav className="navbar-section">
         <span className="navbar-brand">
             Homepage
         </span>
 
-        <ul className="navbar-menu">
+        <div className="navbar-mobile-menu">
+          <a onClick={() => {
+            setIsNavExpanded(!isNavExpanded)
+          }}>
+            <span class="material-symbols-outlined">
+              menu
+            </span>
+          </a>
+        </div>
+
+        <ul className={
+          isNavExpanded ? "navbar-menu expanded" : "navbar-menu"
+        }>
             <li><a href="#">Home</a></li>
             <li><a href="#">Services</a></li>
             <li><a href="#">Our Works</a></li>
