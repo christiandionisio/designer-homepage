7925ced52578bf81e7fe86ca0eaa67406a1e2945
Christian Dionisio <christiandionisio9432@gmail.com>
2023-01-26 22:52:14 -0500

    services setction size fixed

--- src/components/homepage/ServicesSection.jsx
+++ src/components/homepage/ServicesSection.jsx
@@ -7,62 +7,65 @@ export const ServicesSection = () => {
             We offer high demand services
         </h1>
         
-        <div className='card'>
-            <div className='card-header-icon bg-blue'>
-                <span className="material-symbols-outlined">
-                    edit
-                </span>
+        <div className="service-cards">
+            <div className='card'>
+                <div className='card-header-icon bg-blue'>
+                    <span className="material-symbols-outlined">
+                        edit
+                    </span>
+                </div>
+                <h2 className='card-header-title'>UI/UX Design</h2>
+                <p className='card-body-paragragh'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing 
+                    elit. Sit provident saepe vero beatae, 
+                    pariatur labore, deserunt magni corrupti 
+                    nulla necessitatibus iste odit tempora. 
+                    Beatae animi laudantium non nostrum maiores est.
+                </p>
+                <button className='card-btn-footer'>
+                    Get started
+                </button>
             </div>
-            <h2 className='card-header-title'>UI/UX Design</h2>
-            <p className='card-body-paragragh'>
-                Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Sit provident saepe vero beatae, 
-                pariatur labore, deserunt magni corrupti 
-                nulla necessitatibus iste odit tempora. 
-                Beatae animi laudantium non nostrum maiores est.
-            </p>
-            <button className='card-btn-footer'>
-                Get started
-            </button>
-        </div>
 
-        <div className='card'>
-            <div className='card-header-icon'>
-                <span className="material-symbols-outlined">
-                    code
-                </span>
+            <div className='card'>
+                <div className='card-header-icon'>
+                    <span className="material-symbols-outlined">
+                        code
+                    </span>
+                </div>
+                <h2 className='card-header-title'>Front End</h2>
+                <p className='card-body-paragragh'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing 
+                    elit. Sit provident saepe vero beatae, 
+                    pariatur labore, deserunt magni corrupti 
+                    nulla necessitatibus iste odit tempora. 
+                    Beatae animi laudantium non nostrum maiores est.
+                </p>
+                <button className='card-btn-footer'>
+                    Get started
+                </button>
             </div>
-            <h2 className='card-header-title'>Front End</h2>
-            <p className='card-body-paragragh'>
-                Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Sit provident saepe vero beatae, 
-                pariatur labore, deserunt magni corrupti 
-                nulla necessitatibus iste odit tempora. 
-                Beatae animi laudantium non nostrum maiores est.
-            </p>
-            <button className='card-btn-footer'>
-                Get started
-            </button>
-        </div>
 
-        <div className='card'>
-            <div className='card-header-icon bg-red'>
-                <span className="material-symbols-outlined">
-                    storage
-                </span>
+            <div className='card'>
+                <div className='card-header-icon bg-red'>
+                    <span className="material-symbols-outlined">
+                        storage
+                    </span>
+                </div>
+                <h2 className='card-header-title'>Back End</h2>
+                <p className='card-body-paragragh'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing 
+                    elit. Sit provident saepe vero beatae, 
+                    pariatur labore, deserunt magni corrupti 
+                    nulla necessitatibus iste odit tempora. 
+                    Beatae animi laudantium non nostrum maiores est.
+                </p>
+                <button className='card-btn-footer'>
+                    Get started
+                </button>
             </div>
-            <h2 className='card-header-title'>Back End</h2>
-            <p className='card-body-paragragh'>
-                Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Sit provident saepe vero beatae, 
-                pariatur labore, deserunt magni corrupti 
-                nulla necessitatibus iste odit tempora. 
-                Beatae animi laudantium non nostrum maiores est.
-            </p>
-            <button className='card-btn-footer'>
-                Get started
-            </button>
         </div>
+        
     </div>
   )
 }
